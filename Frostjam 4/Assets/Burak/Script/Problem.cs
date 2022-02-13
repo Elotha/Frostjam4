223edc12f5f9a3f9d8d5dc71efd4650dea51a5b0
burakkayalak <burakkayalak@gmail.com>
2022-02-13 18:24:26 +0300

    Problem and Robot indicator images added

--- Frostjam 4/Assets/Burak/Script/Problem.cs
+++ Frostjam 4/Assets/Burak/Script/Problem.cs
@@ -6,16 +6,26 @@ public class Problem : MonoBehaviour
 {
     [SerializeField] public Vector2Int gridPosition;
     [SerializeField] public float RemainingProblemInSeconds;
+    [SerializeField] public Transform remainigPartIndicator;
     public bool IsAvailable = true;
+
+    private float startRemainingtime;
 
     void Start()
     {
         GridManager.Instance.problemsList.Add(this);
+        startRemainingtime = RemainingProblemInSeconds;
     }
 
     public void ReduceProblem()
     {
         RemainingProblemInSeconds -= Time.deltaTime;
+        remainigPartIndicator.localScale = new Vector3(RemainingProblemInSeconds / startRemainingtime, 
+                            remainigPartIndicator.localScale.y, remainigPartIndicator.localScale.z);
+        remainigPartIndicator.localPosition = new Vector3(-0.49f + RemainingProblemInSeconds / startRemainingtime / 2f,
+                            remainigPartIndicator.localPosition.y, remainigPartIndicator.localPosition.z);
+
+
         if (RemainingProblemInSeconds < 0)
         {
             GridManager.Instance.problemsList.Remove(this);
