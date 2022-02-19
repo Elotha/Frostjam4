1ecbe4d428f73093b7ce2cd4cf5e83a8f93ee3b7
mSafa <safaktrcgl@gmail.com>
2022-02-19 16:54:22 +0300

    Add Animator Base for Programs

--- Frostjam 4/Assets/Scripts/ProgrammeAnimator.cs
+++ Frostjam 4/Assets/Scripts/ProgrammeAnimator.cs
@@ -8,6 +8,7 @@ public class ProgrammeAnimator : MonoBehaviour
     private Vector2Int direction;
     private Animator _animator;
     private Robot _robot;
+    private ProgramState _state;
 
     private void Start()
     {
@@ -17,36 +18,44 @@ public class ProgrammeAnimator : MonoBehaviour
 
     private void Update()
     {
+        _state = _robot.programState;
         direction = _robot.direction;
-        if (direction.x == 0 && direction.y == 0)
+        if (_state == ProgramState.Moving)
         {
-            _animator.SetBool("isMoving", false);
-            _animator.SetFloat("HorizontalSpeed", 0f);
-            _animator.SetFloat("VerticalSpeed", 0f);
+            if (direction.x == 0 && direction.y == 0)
+            {
+                _animator.SetBool("isMoving", false);
+                _animator.SetFloat("HorizontalSpeed", 0f);
+                _animator.SetFloat("VerticalSpeed", 0f);
+            }
+            else
+            {
+                _animator.SetBool("isMoving", true);
+                if (direction.x > 0)
+                {
+                    _animator.SetFloat("HorizontalSpeed", 1f);
+                    _animator.SetFloat("VerticalSpeed", 0f);
+                }
+                else if (direction.x < 0)
+                {
+                    _animator.SetFloat("HorizontalSpeed", -1f);
+                    _animator.SetFloat("VerticalSpeed", 0f);
+                }
+                else if (direction.y > 0)
+                {
+                    _animator.SetFloat("VerticalSpeed", -1f);
+                    _animator.SetFloat("HorizontalSpeed", 0f);
+                }
+                else if (direction.y < 0)
+                {
+                    _animator.SetFloat("VerticalSpeed", 1f);
+                    _animator.SetFloat("HorizontalSpeed", 0f);
+                }
+            }
         }
         else
         {
-            _animator.SetBool("isMoving", true);
-            if (direction.x > 0)
-            {
-                _animator.SetFloat("HorizontalSpeed", 1f);
-                _animator.SetFloat("VerticalSpeed", 0f);
-            }
-            else if (direction.x < 0)
-            {
-                _animator.SetFloat("HorizontalSpeed", -1f);
-                _animator.SetFloat("VerticalSpeed", 0f);
-            }
-            else if (direction.y > 0)
-            {
-                _animator.SetFloat("VerticalSpeed", -1f);
-                _animator.SetFloat("HorizontalSpeed", 0f);
-            }
-            else if (direction.y < 0)
-            {
-                _animator.SetFloat("VerticalSpeed", 1f);
-                _animator.SetFloat("HorizontalSpeed", 0f);
-            }
+            _animator.SetBool("isMoving", false);
         }
     }
 }
